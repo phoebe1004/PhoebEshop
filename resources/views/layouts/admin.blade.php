b4045bbb75d612b73dfabcc351b4b17544bc9951
Phoebe Claire Lanquino <89074978+phoebe1004@users.noreply.github.com>
2023-03-30 14:14:50 +0800

    #setup Creative Tim Layout Dashboard #Added Laravel UI #Added User roles 1 for admin and 0 for default

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'PhoebEshop') }}</title>
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+    <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet">
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    <!-- Scripts -->
+    <script src="{{ asset('admin/js/jquery.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/popper.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/bootstrap-material-design.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/perfect-scrollbar.jquery.min.js') }}" defer></script>
+</body>
+
+</html>
